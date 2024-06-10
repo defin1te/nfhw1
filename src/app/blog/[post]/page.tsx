293c8604b5c1cdc3c30bd3182ba4cd487f8ea350
Aldiyar Muratkhanov <aldiyarmuratkhanov700@gmail.com>
2024-06-10 23:57:10 +0500

    deployment error fix

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -1,24 +1,30 @@
-// app/blog/[post]/page.tsx
 'use client';
 
-import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation'; // Import useParams from next/navigation
 import { data } from '@/app/data/data';
 import Head from 'next/head';
 import Link from 'next/link';
 
+interface Post {
+    id: number;
+    date: string;
+    author: string;
+    title: string;
+    description: string;
+}
+
 export default function BlogPost() {
-    const pathname = usePathname();
-    const postSlug = pathname.split('/').pop();
-    const [postData, setPostData] = useState(null);
+    const { post } = useParams();
+    const [postData, setPostData] = useState<Post | null>(null);
 
     useEffect(() => {
-        if (postSlug && typeof postSlug === 'string') {
-            const id = parseInt(postSlug.split('-')[0]);
-            const foundPost = data.find((post) => post.id === id);
+        if (post && typeof post === 'string') {
+            const id = parseInt(post.split('-')[0]);
+            const foundPost = data.find(post => post.id === id) || null;
             setPostData(foundPost);
         }
-    }, [postSlug]);
+    }, [post]);
 
     if (!postData) {
         return <div>Post not found</div>;
@@ -31,7 +37,7 @@ export default function BlogPost() {
                 <meta name="description" content={postData.description} />
             </Head>
             <div className="container flex-center">
-                <div className="post-card">
+                <div className='post-card'>
                     <h1>{postData.title}</h1>
                     <p>{postData.description}</p>
                     <h4>By: {postData.author}</h4>
